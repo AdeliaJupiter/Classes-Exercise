8bede3adbe0ff1c42a8e9bcee73d70bbf00f5b51
Adelia Johnson <132602211+AdeliaJupiter@users.noreply.github.com>
2024-06-10 12:58:22 -0400

    finished todo

---
+++ Classes/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        //Create a Make property of type string that is public
+        public string Make { get; set; }
+
+        //Create a Model property of type string that is public
+        public string Model { get; set; }
+
+        //Create a Year property of type int that is public
+        public int Year { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a new instance of the car class
+            Car car = new Car();
+
+            //Set values in the properties for the object
+            car.Make = "Cheverolet";
+            car.Model = "Impala";
+            car.Year = 2007;
+
+            //Print the values of each property to Console
+            Console.WriteLine(car.Make);
+            Console.WriteLine(car.Model);
+            Console.WriteLine(car.Year);
         }
     }
 }
